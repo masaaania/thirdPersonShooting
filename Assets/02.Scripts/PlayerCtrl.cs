5f05776ab12e76276422acb94a5bf0b724519ed4
tashiro_kazuhiro <tashiro_kazuhiro@cyberagent.co.jp>
2014-09-19 18:28:39 +0900

    マウスの位置に応じて回転する。

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,9 +6,13 @@ public class PlayerCtrl : MonoBehaviour {
 	private float h = 0.0f;
 	private float v = 0.0f;
 
+	private Transform trans;
+
+	public float rotSpeed = 100.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		trans = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +23,6 @@ public class PlayerCtrl : MonoBehaviour {
 		Debug.Log("H = " + h.ToString());
 		Debug.Log("V = " + v.ToString());
 
-	
+		trans.Rotate (Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
 	}
 }
