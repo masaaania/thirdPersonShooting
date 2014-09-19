b482163054108c8742e04a7cdc8616edef7668ec
tashiro_kazuhiro <tashiro_kazuhiro@cyberagent.co.jp>
2014-09-19 11:27:42 +0900

    playerのモデルとplayerCtrlの追加

---
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+
+		Debug.Log("H = " + h.ToString());
+		Debug.Log("V = " + v.ToString());
+
+	
+	}
+}
