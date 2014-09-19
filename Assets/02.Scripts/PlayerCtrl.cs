26319d3f3a498ec9db1414b5740e56e53ce2ef53
tashiro_kazuhiro <tashiro_kazuhiro@cyberagent.co.jp>
2014-09-19 18:19:53 +0900

    前後に動く

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,9 +6,12 @@ public class PlayerCtrl : MonoBehaviour {
 	private float h = 0.0f;
 	private float v = 0.0f;
 
+	private Transform tr;
+	public float moveSpeed = 10.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		tr = GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +22,6 @@ public class PlayerCtrl : MonoBehaviour {
 		Debug.Log("H = " + h.ToString());
 		Debug.Log("V = " + v.ToString());
 
-	
+		tr.Translate(Vector3.forward * moveSpeed * v * Time.deltaTime, Space.Self);
 	}
 }
