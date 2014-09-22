1417e1e49c30059ef3275668861ad1dc392d9085
tashiro_kazuhiro <tashiro_kazuhiro@cyberagent.co.jp>
2014-09-22 19:19:26 +0900

    155pageまで。弾丸打てるようになった。

---
+++ Assets/02.Scripts/WallCtrl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCtrl : MonoBehaviour {
+
+	public GameObject sparkEffect;
+
+	void OnCollisionEnter (Collision collision){
+		if(collision.collider.tag == "BULLET"){
+			Vector3 firePos = collision.gameObject.GetComponent<BulletCtrl>().firePos;
+			Vector3 relativePos = firePos - collision.transform.position;
+
+			Object obj = Instantiate(sparkEffect, collision.transform.position, Quaternion.identity);
+			Destroy(obj, 1.0f);
+//			Instantiate(sparkEffect, collision.transform.position, Quaternion.identity);
+			Destroy(collision.gameObject);
+		}
+	}
+}
